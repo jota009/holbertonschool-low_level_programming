03ea68d35a497b650fffe1e0daf2be540f1cda17
josniel <josnielramos@gmail.com>
2025-02-19 13:59:21 -0400

    6-size.c addlineagain

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
+	size_t size_of_int = sizeof(int);
 	printf("float: %zu byte(s)\n", sizeof(float));
 	printf("long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("long int: %zu byte(s)\n" sizeof(long int));
+	printf("long int: %zu byte(s)\n", sizeof(long int));
 	printf("int: %zu byte(s)\n", sizeof(int));
 	printf("char: %zu byte(s)\n", sizeof(char));
 	return (0);
 }
-

40c76fa2f4e038b332c378eba9ab3ef1e320c2ff
josniel <josnielramos@gmail.com>
2025-02-19 13:46:26 -0400

    6-size.c add include stddef.h

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stddef.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+
 	printf("float: %zu byte(s)\n", sizeof(float));
 	printf("long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("long int: %zu byte(s)\n" sizeof(long int));

3367cff8107ad47eaac7e4bbfa6b13d42af54e10
josniel <josnielramos@gmail.com>
2025-02-20 15:47:20 -0400

    1-last_digit las int fixed

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,19 +10,18 @@
 int main(void)
 {
 	int n;
-	int last;
+	int last = 0;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = -last;
 
 	printf("Last digit of %d is %d", n, last);
 
 	if (last > 5)
-		printf("and is greater than 5\n", n);
+		printf("%d and is greater than 5\n", n);
 	else if (last == 0)
-		printf("and is 0\n", n);
+		printf("%d and is 0\n", n);
 	else
-		printf("and is less than 6 and not 0\n", n);
+		printf("%d and is less than 6 and not 0\n", n);
 	return (0);
 }

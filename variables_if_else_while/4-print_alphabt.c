2cc65990391767a0d319f1f73706eaccbe88e5c0
josniel <josnielramos@gmail.com>
2025-02-21 14:50:26 -0400

    4-print_alphabt.c first commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabet in lower case without q and e
+ * Return: value of alphabet
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; <= 'z'; c++)
+
+		if (c != 'q'; &&c != 'e')
+			putchar(c);
+
+	putchar('\n');
+	return (0);
+}
+

4920711c69f8d5ea02438a770c1c143975d2405f
josniel <josnielramos@gmail.com>
2025-02-21 15:37:18 -0400

    5-print_numbers first commit

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Printing single digits in base form
+ * Return: Value of base 10 digits
+ */
+
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+	{
+		printf("%d", s);
+	}
+	printf("\n");
+
+	return (0);
+}

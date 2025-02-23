380ebe16b69ebc931a25c9c8ec8892da36d635c3
josniel <josnielramos@gmail.com>
2025-02-23 00:40:09 -0400

    9-print_comb.c fixed  printing with cat -e

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,10 +13,13 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
-	}
 
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }

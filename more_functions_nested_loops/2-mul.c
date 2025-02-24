a31d0da8c7ff1401d8785a07d6b18548cd0f82f6
josniel <josnielramos@gmail.com>
2025-02-24 15:47:39 -0400

    2-mul.c first commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Functions that multiplies two integers
+ *
+ * @a: First integer
+ * @b: second integer
+ *
+ * Return: Product of 'a' and 'b'
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

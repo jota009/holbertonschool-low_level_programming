726d7f51204d79ef04894ea5ca2dc2a54137fea6
josniel <josnielramos@gmail.com>
2025-02-24 16:17:13 -0400

    3-print_numbers.c first commit

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Printing numbers 0 to 9 followed by new line
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c != 9);
+	}
+	_putchar('\n');
+}

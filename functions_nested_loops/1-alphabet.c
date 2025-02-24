dc7b7ad9a869b370a75a98a7e941b714e313f8de
josniel <josnielramos@gmail.com>
2025-02-23 23:05:34 -0400

    1-alphabet first commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Printing the alphabet
+ */
+void print_alphabet(void)
+{
+	int abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		_putchar(abc);
+	_putchar('\n');
+}

77fae7dbf9f91ed39e40846457841c45a98e7641
josniel <josnielramos@gmail.com>
2025-03-09 13:18:05 -0400

    1-args first commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number if arguments passed into it
+ * @argc: Number of command-line arguments
+ * @argv: Array of the commad-line arguments
+ *
+ * Return: 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc -1);
+	return (0);
+}

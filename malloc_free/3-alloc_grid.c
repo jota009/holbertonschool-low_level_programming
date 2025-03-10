8ef9d121ec947069d692689eb7508bc97a99165c
josniel <josnielramos@gmail.com>
2025-03-10 16:25:20 -0400

    3-alloc_grid.c betty fixed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,25 +9,31 @@
  * Return: Pointer to the 2D array, or NULL on failure
  */
 
-int **alloc_grid(int width, int height) 
+int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0) return NULL;
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    grid = malloc(height * sizeof(int *));
-    if (!grid) return NULL;
+	grid = malloc(height * sizeof(int *));
 
-    for (i = 0; i < height; i++) {
-        grid[i] = malloc(width * sizeof(int));
-        if (!grid[i]) {
-            for (j = 0; j < i; j++) free(grid[j]);
-            free(grid);
-            return NULL;
-        }
-        for (j = 0; j < width; j++) grid[i][j] = 0;
-    }
+	if (!grid)
+		return (NULL);
 
-    return grid;
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (!grid[i])
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }

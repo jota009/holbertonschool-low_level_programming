df67f796737ca1b420ad09806ca59e6a396c89e0
josniel <josnielramos@gmail.com>
2025-03-10 15:26:58 -0400

    2-str_concat commit strlen fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,7 +12,8 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
-	size_t len_s1, len_s2;
+	int len_s1 = 0, len_s2 = 0;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,15 +21,21 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
+	while (s1[len_s1] != '\0')
+		len_s1++;
+
+	while (s2[len_s2] != '\0')
+		len_s2++;
 
 	new_str = malloc(len_s1 + len_s2 + 1);
 	if (new_str == NULL)
 		return (NULL);
 
-	memcpy(new_str, s1, len_s1);
-	memcpy(new_str + len_s1, s2, len_s2 + 1);
+	for(i = 0; i < len_s1; i++)
+		new_str[i] = s1[i];
+
+	for (j = 0; j <= len_s2; j++)
+		new_str[i + j] = s2[j];
 
 	return (new_str);
 }

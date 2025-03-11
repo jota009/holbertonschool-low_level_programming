cc674b597deb49eb5991a5b231ce1da9830b5950
josniel <josnielramos@gmail.com>
2025-03-11 12:21:34 -0400

    0-malloc_checked first commit

---
+++ more_malloc_free/0-malloc_checked
@@ -0,0 +1,1 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-malloc_checked.c -o a
---
+++ more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif

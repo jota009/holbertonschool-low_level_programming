7cc601cc3cc5561c6e25caf8a9e8bb484fcd82d1
josniel <josnielramos@gmail.com>
2025-03-14 11:37:33 -0400

    4-new_dog.c first commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * my_strlen - Custom fucntion to calculate string lenght
+ * @str: Input string
+ *
+ * Return: Lenght of the string
+ */
+
+int my_strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * my_strcpy -  Custom function that copies a string
+ * @src: Pointer to the source
+ * @dest: Destination the string will be copied to
+ *
+ * Return: Copied string in new location
+ */
+
+void my_strcpy(char *dest, const char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
 
 /**
  * new_dog - Function that creates a new dog
@@ -18,26 +54,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = malloc(strlen(name) + 1);
+
+	new_dog->name = malloc(my_strlen(name) + 1);
 
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
-				return (NULL);
-				}
+		return (NULL);
+	}
 
-				new_dog->owner = malloc(strlen(owner) + 1);
+	new_dog->owner = malloc(my_strlen(owner) + 1);
 
-				if (new_dog->owner == NULL)
-				{
-				free(new_dog->name);
-				free(new_dog);
-				return (NULL);
-				}
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 
-				strcpy(new_dog->name, name);
-				new_dog->age = age;
-				strcpy(new_dog->owner, owner);
+	my_strcpy(new_dog->name, name);
+	new_dog->age = age;
+	my_strcpy(new_dog->owner, owner);
 
-				return (new_dog);
-				}
+	return (new_dog);
+
+}

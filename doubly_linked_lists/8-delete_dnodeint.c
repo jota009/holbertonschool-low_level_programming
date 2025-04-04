3ff5af42bd34dda27b67ad6a48c133e189d48e70
josniel <josnielramos@gmail.com>
2025-04-04 14:45:15 -0400

    8-delete_dnodeint.c first output fix

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -26,7 +26,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(current);
-		return (-1);
+		return (1);
 	}
 
 	for (count = 0; current != NULL && count < index; count++)
